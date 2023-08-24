e6e5cf5ca200803eb07d920a376b2500e50cdca3
louisboilard <louis@gravitysketch.com>
2023-08-24 17:52:13 -0400

    use binary notation for clearer mapping with textbook encoding

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,6 @@ use std::str::FromStr;
 // Instruction size in bytes
 const INSTRUCTION_SIZE: usize = 2;
 
-type InstructionTuple = (u8, u8);
-
 /* ===============================================
 *  =============== BIT MASKS =====================
 *  ===============================================
@@ -88,7 +86,7 @@ impl OpCode {
     /// Returns a byte representing the 6bit for the opcode.
     pub fn to_byte(&self) -> Option<u8> {
         match self {
-            Self::MOV => Some(136),
+            Self::MOV => Some(0b_10_001000),
             _ => None,
         }
     }
@@ -184,33 +182,33 @@ impl Register {
     /// Outputs which register the wide and register(REG/RM) flags represent.
     fn from_flags(wide_flag: u8, register_flag: u8) -> Self {
         // Wide is a bitflag of size 1. reg is 3bits wide.
-        if wide_flag > 1 || register_flag > 7 {
+        if wide_flag > 0b_0000_0111 || register_flag > 0b_0000_0111 {
             return Self::UNKNOWN;
         }
 
-        // When wide == 1, the instruction is on 16bits.
+        // When wide == 1, instruction is on 16bits.
         match wide_flag {
             1 => match register_flag {
-                0 => Self::Ax,
-                1 => Self::Cx,
-                2 => Self::Dx,
-                3 => Self::Bx,
-                4 => Self::Sp,
-                5 => Self::Bp,
-                6 => Self::Si,
-                7 => Self::Di,
+                0b_0000_0000 => Self::Ax,
+                0b_0000_0001 => Self::Cx,
+                0b_0000_0010 => Self::Dx,
+                0b_0000_0011 => Self::Bx,
+                0b_0000_0100 => Self::Sp,
+                0b_0000_0101 => Self::Bp,
+                0b_0000_0110 => Self::Si,
+                0b_0000_0111 => Self::Di,
                 _ => Self::UNKNOWN,
             },
             // wide == 0 -> instruction is on 8bits.
             0 => match register_flag {
-                0 => Self::Al,
-                1 => Self::Cl,
-                2 => Self::Dl,
-                3 => Self::Bl,
-                4 => Self::Ah,
-                5 => Self::Ch,
-                6 => Self::Dh,
-                7 => Self::Bh,
+                0b_0000_0000 => Self::Al,
+                0b_0000_0001 => Self::Cl,
+                0b_0000_0010 => Self::Dl,
+                0b_0000_0011 => Self::Bl,
+                0b_0000_0100 => Self::Ah,
+                0b_0000_0101 => Self::Ch,
+                0b_0000_0110 => Self::Dh,
+                0b_0000_0111 => Self::Bh,
                 _ => Self::UNKNOWN,
             },
             _ => Self::UNKNOWN,
@@ -566,15 +564,15 @@ pub fn read_instructions(instructions: &[u8]) -> Result<Vec<Instruction>, String
                         instructions[index + 1],
                     ) {
                         Ok(i) => {
-                                index += i.width;
-                                instructions_vec.push(Instruction::RegisterToRegister(i));
-                            },
+                            index += i.width;
+                            instructions_vec.push(Instruction::RegisterToRegister(i));
+                        }
                         Err(e) => return Err(e),
                     }
                 }
                 _ => return Err("Unknown instruction".to_owned()),
-            }
-            None => return Err("Could not parse instruction".to_owned())
+            },
+            None => return Err("Could not parse instruction".to_owned()),
         };
     }
 
