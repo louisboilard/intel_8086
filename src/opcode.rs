60920b2ff066d8f9ed37e840b6c55531eccdcd80
louisboilard <louis@gravitysketch.com>
2023-10-21 21:18:52 -0400

    opcode level support for sub

--- src/opcode.rs
+++ src/opcode.rs
@@ -24,13 +24,17 @@ pub enum OpCode {
     Mov,
     /// Add DST, SRC (addition)
     Add,
+    /// Sub DST, SRC (substraction)
+    Sub,
     /// Represents an unrecognized instruction.
     Unknown,
 }
 
 impl OpCode {
     /// Returns a mnemonic variant based on the byte's value.
-    pub fn from_binary(val: u8) -> Option<(Self, OpKind)> {
+    /// second_byte required for certain type of instruction
+    /// (namely immediate->register or memory)
+    pub fn from_binary(val: u8, second_byte: Option<u8>) -> Option<(Self, OpKind)> {
         // register to register instructions have 6 bits op
         let register_to_register_op: u8 = val >> 2;
 
@@ -43,13 +47,34 @@ impl OpCode {
             // MOV is 100010 (34) with 6bits
             0b_0010_0010 => Some((Self::Mov, OpKind::MemoryOrRegToReg)),
             0b_0000_0000 => Some((Self::Add, OpKind::MemoryOrRegToReg)),
+            0b_0000_1010 => Some((Self::Sub, OpKind::MemoryOrRegToReg)),
             _ => {
-                // check if facing an immediate register OP
-                match immediate_register_op {
-                    0b_0000_1011 => Some((Self::Mov, OpKind::ImmediateRegister)),
-                    _ => match immediate_register_or_memory_op {
-                        0b_0011_0001 => Some((Self::Mov, OpKind::ImmediateToRegisterOrMemory)),
-                        0b_0010_0000 => Some((Self::Add, OpKind::ImmediateToRegisterOrMemory)),
+                match immediate_register_or_memory_op {
+                    0b_0011_0001 => Some((Self::Mov, OpKind::ImmediateToRegisterOrMemory)),
+                    // Sub, SBB, CMP.. have the same first byte
+                    // match against second byte's middle 3 bits
+                    // (xx_YYY_xxx) to differentiate
+                    0b_0010_0000 => {
+                        match second_byte {
+                            Some(b) => {
+                                match b & 0b_0011_1000 {
+                                    // 3 middle bits are 0s for add
+                                    0b_0000_0000 => {
+                                        Some((Self::Add, OpKind::ImmediateToRegisterOrMemory))
+                                    }
+                                    0b_0011_1000 => None, // TODO: CMP
+                                    // 3 middle bits are 1s for sub
+                                    0b_0010_1000 => {
+                                        Some((Self::Sub, OpKind::ImmediateToRegisterOrMemory))
+                                    }
+                                    _ => None,
+                                }
+                            }
+                            _ => Some((Self::Unknown, OpKind::ImmediateToRegisterOrMemory)),
+                        }
+                    }
+                    _ => match immediate_register_op {
+                        0b_0000_1011 => Some((Self::Mov, OpKind::ImmediateRegister)),
                         _ => None,
                     },
                 }
@@ -63,6 +88,7 @@ impl OpCode {
             OpKind::MemoryOrRegToReg => match self {
                 Self::Mov => Some(0b_1000_1000),
                 Self::Add => Some(0b_0000_0000),
+                Self::Sub => Some(0b_0010_1000),
                 _ => None,
             },
             OpKind::ImmediateRegister => match self {
@@ -72,6 +98,7 @@ impl OpCode {
             OpKind::ImmediateToRegisterOrMemory => match self {
                 Self::Mov => Some(0b_1100_0011),
                 Self::Add => Some(0b_1000_0000),
+                Self::Sub => Some(0b_1000_0000),
                 _ => None,
             },
             _ => None,
